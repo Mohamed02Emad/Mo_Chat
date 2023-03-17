1a6cdfbf55b0ea4a0243e5a7b408c368c2212bda
MohamedEmad <aleslam12345@gmail.com>
2023-03-17 21:15:17 +0200

    add login ui

---
+++ app/src/main/java/com/mo_chatting/chatapp/presentation/home/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.mo_chatting.chatapp.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mo_chatting.chatapp.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
--- app/src/main/java/com/mo_chatting/chatapp/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/mo_chatting/chatapp/presentation/login/LoginFragment.kt
@@ -1,18 +1,20 @@
-package com.mo_chatting.chatapp
+package com.mo_chatting.chatapp.presentation.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mo_chatting.chatapp.R
 
-class HomeFragment : Fragment() {
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
 }
