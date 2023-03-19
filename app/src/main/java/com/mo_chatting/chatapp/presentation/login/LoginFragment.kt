3528a6393b5746b4a06ffe3f13c5c60e576895b1
MohamedEmad <aleslam12345@gmail.com>
2023-03-19 11:21:11 +0200

    add auth activity and it's navigation

--- app/src/main/java/com/mo_chatting/chatapp/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/mo_chatting/chatapp/presentation/login/LoginFragment.kt
@@ -1,20 +1,66 @@
 package com.mo_chatting.chatapp.presentation.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mo_chatting.chatapp.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.mo_chatting.chatapp.MainActivity
+import com.mo_chatting.chatapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    lateinit var binding:FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding=FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClicks()
+    }
+
+    private fun setOnClicks() {
+        binding.btnLogin.setOnClickListener {
+            if (isValidAccount()){
+                startActivity(Intent(requireActivity(),MainActivity::class.java))
+                requireActivity().finish()
+            }else{
+                //todo add logic
+                showToast("Error")
+            }
+        }
+
+        binding.tvSignUp.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment())
+        }
+
+        binding.tvForgotPassword.setOnClickListener {
+            showToast("Soon")
+        }
+        binding.loginWithFacebook.setOnClickListener {
+            showToast("Soon")
+        }
+        binding.loginWithGoogle.setOnClickListener {
+            showToast("Soon")
+        }
+    }
+
+
+    private fun isValidAccount(): Boolean {
+       //todo add logic
+        return true
+    }
+
+    private fun showToast(s:String){
+        Toast.makeText(requireActivity(),s,Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/mo_chatting/chatapp/presentation/signUp/SignUpFragment.kt
+++ app/src/main/java/com/mo_chatting/chatapp/presentation/signUp/SignUpFragment.kt
@@ -1,19 +1,63 @@
 package com.mo_chatting.chatapp.presentation.signUp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mo_chatting.chatapp.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.mo_chatting.chatapp.MainActivity
+import com.mo_chatting.chatapp.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    lateinit var binding: FragmentSignUpBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        binding = FragmentSignUpBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClicks()
+    }
+
+    private fun setOnClicks() {
+        binding.btnBackArrow.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.tvLogin.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.btnSignUp.setOnClickListener {
+            if (accountCreated()) {
+                startActivity(Intent(requireActivity(),MainActivity::class.java))
+            } else {
+                //todo add loigc
+                Toast.makeText(requireContext(), "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.loginWithFacebook.setOnClickListener {
+            showToast("Soon")
+        }
+        binding.loginWithGoogle.setOnClickListener {
+            showToast("Soon")
+        }
+    }
+
+    private fun accountCreated(): Boolean {
+        // TODO: add logic
+        return true
+    }
+
+    private fun showToast(s:String)=
+        Toast.makeText(requireActivity(),s,Toast.LENGTH_SHORT).show()
+
 
 }
