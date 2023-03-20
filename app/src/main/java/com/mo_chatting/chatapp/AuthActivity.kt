21e21be46a5833331751a2620dd5a432508a3d67
MohamedEmad <aleslam12345@gmail.com>
2023-03-21 01:04:49 +0200

    add edit profile image

--- app/src/main/java/com/mo_chatting/chatapp/AuthActivity.kt
+++ app/src/main/java/com/mo_chatting/chatapp/AuthActivity.kt
@@ -1,14 +1,48 @@
 package com.mo_chatting.chatapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.facebook.FacebookSdk
 
 class AuthActivity : AppCompatActivity() {
+
+    private val pushPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        if (granted) {
+            //Toast.makeText(this, "granted", Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkCameraPermission()
         setContentView(R.layout.activity_auth)
         //FacebookSdk.sdkInitialize(FacebookSdk.getApplicationContext())
+        requestForPermission()
+    }
 
+
+    private fun requestForPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pushPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                == PackageManager.PERMISSION_DENIED
+            ) {
+                val permission =
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permission, 112)
+            }
+        }
     }
 }
--- app/src/main/java/com/mo_chatting/chatapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/mo_chatting/chatapp/presentation/home/HomeFragment.kt
@@ -1,17 +1,28 @@
 package com.mo_chatting.chatapp.presentation.home
 
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.mo_chatting.chatapp.AuthActivity
 import com.mo_chatting.chatapp.R
 import com.mo_chatting.chatapp.databinding.FragmentHomeBinding
@@ -25,6 +36,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private val viewModel: HomeViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,7 +54,15 @@ class HomeFragment : Fragment() {
 
     private fun setUserViews() {
         binding.tvUserName.text = firebaseAuth.currentUser!!.displayName
-        binding.profile.setImageURI(firebaseAuth.currentUser!!.photoUrl)
+
+        val storageRef = FirebaseStorage.getInstance()
+            .getReference("user_images/${firebaseAuth.currentUser!!.uid}")
+        storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+            Glide.with(requireContext()).load(downloadUri).into(binding.profile)
+        }.addOnFailureListener { exception ->
+            Log.e("mohamed", "Error downloading image", exception)
+        }
+
     }
 
     private fun setOnClicks() {
@@ -61,19 +81,18 @@ class HomeFragment : Fragment() {
         binding.fabAdd.setOnClickListener {
 
         }
+
+        binding.tvEditImage.setOnClickListener {
+            showBottomSheet()
+        }
+
     }
 
-    private fun updateUserData(){
+    private fun updateUserData(name: String, imageUri: Uri) {
         firebaseAuth.currentUser?.let { user ->
-            //todo change name
-            val name = "not me"
-            //todo get image from memory
-            val imageUri = Uri.parse("android.resource://${requireActivity().packageName}/${R.drawable.ic_google}")
             val profileUpdates = UserProfileChangeRequest.Builder()
                 .setDisplayName(name)
-                .setPhotoUri(imageUri)
                 .build()
-
             CoroutineScope(Dispatchers.IO).launch {
                 try {
                     user.updateProfile(profileUpdates).await()
@@ -90,8 +109,92 @@ class HomeFragment : Fragment() {
                     }
                 }
             }
-
         }
+
+        val storageRef = FirebaseStorage.getInstance()
+            .getReference("user_images/${firebaseAuth.currentUser!!.uid}")
+        storageRef.putFile(imageUri)
+            .addOnSuccessListener { taskSnapshot ->
+                storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                    val userRef = FirebaseDatabase.getInstance().getReference("users")
+                        .child(firebaseAuth.currentUser!!.uid)
+                    userRef.child("image").setValue(downloadUri.toString())
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle any errors that occurred during the upload
+                Log.e("mohamed", "Error uploading image", exception)
+            }
+
+
     }
 
+
+    private fun showBottomSheet() {
+        val dialog = BottomSheetDialog(requireContext())
+        val view = layoutInflater.inflate(R.layout.chose_edit_image, null)
+        val btnCamera: LinearLayout = view.findViewById(R.id.camera_choice)
+        btnCamera.setOnClickListener {
+            startCameraIntent()
+            dialog.dismiss()
+        }
+        val btnGallery: LinearLayout = view.findViewById(R.id.gallery_choice)
+        btnGallery.setOnClickListener {
+            startGalleryIntent()
+            dialog.dismiss()
+        }
+        dialog.setCancelable(true)
+        dialog.setContentView(view)
+        dialog.show()
+    }
+
+
+    private fun startCameraIntent() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From Camera")
+        viewModel.uri.value = requireContext().contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values
+        )
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, viewModel.uri.value)
+
+        cameraResultLauncher.launch(cameraIntent)
+    }
+
+    private fun startGalleryIntent() {
+        val i = Intent().apply {
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+        }
+        galleryResultLauncher.launch(i)
+    }
+
+    private val galleryResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                viewModel.uri.value = data!!.data
+                binding.profile.setImageURI(viewModel.uri.value)
+
+                updateUserData(
+                    firebaseAuth.currentUser!!.displayName.toString(),
+                    viewModel.uri.value!!
+                )
+            }
+        }
+
+    private val cameraResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (viewModel.uri.value != null)
+                    binding.profile.setImageURI(viewModel.uri.value)
+                updateUserData(
+                    firebaseAuth.currentUser!!.displayName.toString(),
+                    viewModel.uri.value!!
+                )
+            }
+        }
 }
