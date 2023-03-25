b348cee922ea6361fe1a80ffabc630feef642eac
MohamedEmad <aleslam12345@gmail.com>
2023-03-25 02:37:17 +0200

    inject firebase auth and create empty methods for repository

--- app/src/main/java/com/mo_chatting/chatapp/data/repositories/RoomsRepository.kt
+++ app/src/main/java/com/mo_chatting/chatapp/data/repositories/RoomsRepository.kt
@@ -1,10 +1,28 @@
 package com.mo_chatting.chatapp.data.repositories
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.mo_chatting.chatapp.data.models.Room
 
-class RoomsRepository(val firebaseStore : FirebaseFirestore) {
+class RoomsRepository(val firebaseStore : FirebaseFirestore,val firebaseAuth: FirebaseAuth) {
 
+
+    fun createNewRoom(room:Room){
+
+    }
+
+    fun updateRoom(room: Room){
+
+    }
+
+    fun deleteRoom(room: Room){
+
+    }
+
+    fun getUserRooms(){
+        val user = firebaseAuth.currentUser!!
+        val userId = user.uid
+    }
 
     fun getFakeRoomsList():ArrayList<Room>{
         val list = ArrayList<Room>()
